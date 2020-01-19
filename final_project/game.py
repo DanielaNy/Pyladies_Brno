2dd8b4451b6419d266c144db728b5ccc08d0fe26
DanielaN <daniela.nyblova@gmail.com>
2020-01-19 12:00:25 +0100

    quite working app, however,  with several things omitted

--- final_project/game.py
+++ final_project/game.py
@@ -1,114 +1,139 @@
 import sqlite3
 import os
-from random import choice
+from random import choice, randrange
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from web_app import app, APP_ROOT
+from sqlalchemy.exc import IntegrityError
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+db = SQLAlchemy(app)
 
 filename_txt = "user" + "_file.txt"
-filename_db = "user" + "_file.db"
-
-levels = [0, 1, 2, 0, 0, 1, 0, 0, 0, 1]
-words = {levels[0] : {}, levels[1] : {}, levels[2] : {}}
-chosen = None
+no_of_selected = 5
+#levels = [0, 1, 2, 0, 0, 1, 0, 0, 0, 1]
 chosen_list = []
 
-connection = sqlite3.connect("./user/" + filename_db)
-cursor = connection.cursor()
-    
+class User(db.Model):                       # flask-sglalchemy z class spravi tabulku s tymto menom
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    words = db.relationship('Word', backref='user', lazy=True)
 
-def close_connection():
-    connection.commit()
-    connection.close()
+class Word(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    unknown = db.Column(db.String(100), unique=True, nullable=False)
+    known = db.Column(db.String(100), unique=True, nullable=False)
+    level = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-def create_table():
-    """creates a table 'username' for the user if it was not yet created"""
-    cursor.execute("""CREATE TABLE IF NOT EXISTS username(Unknown_Word TEXT, Known_Word TEXT, Level INTEGER)""")
-    connection.commit()
-    return cursor
+    def __repr__(self):
+        return self.unknown, self.known
+
+def save_uploaded_file():
+    """saves the uploaded file by user into 'user' directory"""
+    target = os.path.join(APP_ROOT, 'user/')
+    print(target)
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for file in request.files.getlist("file"):
+        print(file)
+        filename = "user_file" + ".txt"
+        destination = "/".join([target, filename])
+        file.save(destination)
 
 def new_words_from_text_file():
     """appends new words from the text file to the database, if user_textfile.txt uploaded"""
+    db.create_all()
+
+    def add(db):
+        for line in user_file:     
+            line = line.rstrip()
+            uploaded_words = line.split("; ")       # list
+            try:                                
+                new_word = Word(unknown=uploaded_words[0], known=uploaded_words[1], level=0, user_id=1)
+            except:
+                IndexError
+                pass
+            while True:
+                try:                        
+                    db.session.add(new_word)
+                    db.session.commit()
+                    print("added new word:", new_word.unknown, new_word.known)                          # to check
+                    break
+                except IntegrityError:
+                    db.session().rollback()
+                    already_in_db = Word.query.filter_by(unknown=new_word.unknown).first()
+                    if already_in_db:
+                        print(already_in_db.unknown, already_in_db.known, "deleted")                    # to check
+                        db.session.delete(already_in_db)                                
+                        db.session.commit()
+                        continue
+
+        return user_file
+
     if os.path.exists('./user/user_file.txt'):
         with open("./user/"+filename_txt) as user_file:
-            for line in user_file:     
-                line = line.rstrip()
-                uploaded_words = line.split("; ")       # list
-                cursor.execute("""INSERT INTO username(Unknown_Word, Known_Word, Level) VALUES (?, ?, ?)""", 
-                                                            (uploaded_words[0], uploaded_words[1], int(0)))
-        connection.commit()
-        #os.remove('./user/user_file.txt')
-        print("New words added to the \'username\' table from .txt file:")
-        print(uploaded_words)
-
+            user_file = add(db)
+        os.remove('./user/user_file.txt')
     else:
-        print("No new words were added to the \'username\' table.")
         pass
-    return cursor
+    if len(Word.query.all()) < 5:
+        with open("./sample/exampletxt.txt") as user_file:
+            print("words added from sample:")            
+            user_file = add(db)
+    else:
+        print("No new words were added to the \'Word\' table.")
+        pass
 
-def print_all_words_from_database():
-    print("\n-------------------------- all words in table below ------------------------------")
-    all_words = connection.execute("SELECT Unknown_Word, Known_Word, Level FROM username")
-    for word in all_words:
-        print(str(word)+"all")
-    print("-------------------------- all words in table above ------------------------------\n")
-
-def words_to_dictionaries():
-    """adds the words to dictionaries according to knowledge level --> words = dict"""
-    for idx, words_idx in words.items():
-        #print(str(idx))        
-        words_idx = dict(connection.execute("SELECT Unknown_Word, Known_Word FROM username WHERE Level =?", str(idx)))
-        words[idx] = words_idx
-        # for k, v in words_idx.items():
-        #     print(k, ": ", v)
-    return words
+# def print_all_words_from_database():
+#     print("\n-------------------------- all words in table below ------------------------------")
+#     all_words = Word.query.all()
+#     print("This is in database:")
+#     for w in all_words:
+#         print(w.unknown, w.known, w.level)
+#     print("-------------------------- all words in table above ------------------------------\n")
+#     return all_words
 
 def select_word_to_test():
-    """slects random word from dict "word" according to algorithm"""    
-    for lvl in levels:
-        for k, v in words.items():
-            if k == lvl:
-                print("from level:", lvl)       # to check
-                a = dict(list(words.values())[lvl])
-                try:
-                    chosen = choice(list(a.keys()))
-                    print(chosen)       # this is to be shown on web site
-                    chosen_list.append(chosen)                 
-                except IndexError:
-                    pass
+    """slects random words from the database according to algorithm"""
+    print("---selecting random words---")    
+    q = db.session.query(Word)
+    chosen_list.clear()
+    while len(chosen_list) < 5:  
+        rand = randrange(0, q.count())
+        row = db.session.query(Word)[rand]
+        if (row.unknown, row.known, row.id) not in chosen_list:
+            chosen_list.append((row.unknown, row.known, row.id))
+            continue
+    print(chosen_list)
     return chosen_list
 
-
 def prepare_game():
-    """starts the game - creates the connection to database, import words"""
-    connection = sqlite3.connect("./user/" + filename_db)
-    cursor = connection.cursor()
-    
-
-    cursor = create_table()
-    print_all_words_from_database()             # to check
-    cursor = new_words_from_text_file()
-    words = words_to_dictionaries()
-    print(words)                                # to check
+    """starts the game - creates the connection to database, imports words from text file and chooses list of words for the play"""
+    new_words_from_text_file()
+    #print_all_words_from_database()
     chosen_list = select_word_to_test()
-    print(chosen_list)                          # to check
-    return connection, cursor, words, chosen_list
+    # print(chosen_list)
+    return chosen_list
 
-def play_game():
+def evaluate_game(form_words, word_ids):
+    """checks, if the data input from the user corresponds to the chosen words' translation"""
+    final_list = []
+    idx = 0
+    for w_id in word_ids:
+        orig_word = Word.query.filter_by(id=w_id).first()
+        tup = orig_word.unknown, orig_word.known, form_words[idx], form_words[idx] == orig_word.known
+        final_list.append(tup)
+        idx +=1
+    return final_list
 
-    # END
-    close_connection()
+def end_game():
+    """deletes the database"""
+    os.remove('./db.sqlite3')
 
-
-# MAIN LOGIC
-
-# if __name__ == "__main__":
-#     #cursor.execute("""DROP TABLE IF EXISTS username""")
-#     create_table()
-#     print_all_words_from_database()             # to check
-#     new_words_from_text_file()
-#     words_to_dictionaries()
-#     print(words)                                # to check
-#     select_word_to_test()
-#     print(chosen)
-#     # END
-#     close_connection()
-
-
+if __name__ == "__main__":
+    new_words_from_text_file()
+    #print_all_words_from_database()
+    chosen_list  = select_word_to_test()
--- final_project/web_app.py
+++ final_project/web_app.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from random import randint
 import os
-import game
+
+from wtforms import Form, BooleanField, StringField, SubmitField, PasswordField, HiddenField, validators       # to be checked
+from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.config["SECRET_KEY"] = "supersecretkey"                 # wtforms need it
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 bootstrap = Bootstrap(app)
 
-print(game.chosen)                  # to check
+import game
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
@@ -18,7 +24,7 @@ def index():
 
 @app.route("/how_to/")
 def how_to():
-    return render_template("how_to.html")
+    return render_template("how_to.html",no_of_selected=game.no_of_selected)
 
 @app.route("/exampletxt/")
 def exampletxt():
@@ -32,28 +38,50 @@ def game_web():
 def upload():
     return render_template("upload.html")
 
-@app.route("/play/")
-def play():
-    #game.print_all_words_from_database()           ???????
-    return render_template("play.html")
-
 @app.route("/upload_completed/", methods=['POST'])
 def upload_complete():
-    target = os.path.join(APP_ROOT, 'user/')
-    print(target)
+    """"""
+    game.save_uploaded_file()
 
-    if not os.path.isdir(target):
-        os.mkdir(target)
+    return render_template("upload_completed.html")   
 
-    for file in request.files.getlist("file"):
-        print(file)
-        filename = "user_file" + ".txt"
-        destination = "/".join([target, filename])
-        file.save(destination)
-    return render_template("upload_completed.html")
+@app.route("/play/", methods=['GET'])
+def play():
+    chosen_list = game.prepare_game()
+    
+    class TranslationForm(FlaskForm):
+        word1 = StringField(chosen_list[0][0])
+        word2 = StringField(chosen_list[1][0])
+        word3 = StringField(chosen_list[2][0])
+        word4 = StringField(chosen_list[3][0])
+        word5 = StringField(chosen_list[4][0])
+        word_ids = HiddenField()
+        submit = SubmitField('Submit')
 
+    chosen_word_ids = ','.join(str(word[2]) for word in chosen_list)
+    form = TranslationForm(word_ids= chosen_word_ids)
+
+    return render_template("play.html", chosen_list=chosen_list, form=form)
+
+
+@app.route('/result/', methods = ['POST'])
+def result():
+    points = 0
+    form_words = [request.form["word1"], request.form["word2"], request.form["word3"], request.form["word4"], request.form["word5"]]
+    word_ids = request.form["word_ids"].split(",")
+    final_list = game.evaluate_game(form_words, word_ids)
+    for final in final_list: 
+        if final[3] == True: points += 1
+    print("Final_list:", final_list)
+    return render_template("result.html", final_list=final_list, points=points)
+
+     
+@app.route('/end/')
+def end():
+    game.end_game()
+    return render_template("end.html")
 
 if __name__ == "__main__":
     app.run()
-    #app.run(host="0.0.0.0", port="5000")
-    #app.jinja_env.globals.update(play=play)
+    # app.run(host="0.0.0.0", port="5000")
+    
